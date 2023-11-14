188a4d13f7cc29b8a907961ca934eca6895c9c14
Binary <93232046+biney999@users.noreply.github.com>
2023-11-14 16:20:52 +1000

    Update NotFound.jsx
    removed incorrect link

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,19 +2,6 @@ import { Link } from "react-router-dom";
 // import "../404.css";
 
 
-// export default function NotFound() {
-// return (
-//     <div className="container">
-//       {/* Replace "your-logo.png" with the actual path to your circle logo */}
-//       <img src="your-logo.png" alt="Logo" className="logo" />
-//       <h1 className="error-code">404</h1>
-//       <p className="error-message">Page Not Found</p>
-//     </div>
-//   );
-// }
-
-
-
 import "../404.css";
 
 const NotFound = () => {
